2d28a3b23a2147c24863a2fc1d469eb17da6920f
HUY_LINUX <quochuylv304@gmail.com>
2026-02-26 22:47:20 +0700

    Fix 000 files

---
+++ 000_Bounded_Buffer/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/quochuylv304/001_Concurrency_Programming/000_Bounded_Buffer/src/bounded_buffer.cpp" "/home/quochuylv304/001_Concurrency_Programming/000_Bounded_Buffer/build/CMakeFiles/main.dir/src/bounded_buffer.cpp.o"
+  "/home/quochuylv304/001_Concurrency_Programming/000_Bounded_Buffer/src/main.cpp" "/home/quochuylv304/001_Concurrency_Programming/000_Bounded_Buffer/build/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
